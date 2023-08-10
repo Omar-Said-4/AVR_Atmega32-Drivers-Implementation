c9bc0f2cc030609be036b356f444c59459a5e012
omar said <87082462+Omar-Said-4@users.noreply.github.com>
2023-08-11 01:39:25 +0300

    update macros

--- MCAL/01- DIO/DIO_INTERFACE.h
+++ MCAL/01- DIO/DIO_INTERFACE.h
@@ -1,10 +1,3 @@
-/********************************************************/
-/*    SWC: DIOS                                         */
-/*    Author: Omar Said                                 */
-/*    Version : V1.0                                    */
-/*    Description : SWC for Digital Input/Output        */ 
-/********************************************************/
-
 #ifndef DIO_INTERFACE_H
 #define DIO_INTERFACE_H
 #include"STD_TYPES.h"
@@ -14,6 +7,18 @@
 #define LOW 0
 #define PORT_OUTPUT 255
 #define PORT_INPUT 0
+#define IOA 0
+#define IOB 1
+#define IOC 2
+#define IOD 3
+#define PIN0 0
+#define PIN1 1
+#define PIN2 2
+#define PIN3 3
+#define PIN4 4
+#define PIN5 5
+#define PIN6 6
+#define PIN7 7
 void DIO_VidSetPinDirection(u8 port, u8 pin, u8 dir);
 void DIO_VidSetPortDirection(u8 port, u8 dir);
 void DIO_VidSetPinValue(u8 port, u8 pin, u8 val);
